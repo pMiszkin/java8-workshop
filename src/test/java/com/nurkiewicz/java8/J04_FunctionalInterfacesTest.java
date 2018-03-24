3c93695a8ddad2f4dd68d58bc2ce6e734a73b401
Pawel Miszkin <pawel.miszkin@allegro.pl>
2018-03-24 16:40:09 +0100

    J04

--- src/test/java/com/nurkiewicz/java8/J04_FunctionalInterfacesTest.java
+++ src/test/java/com/nurkiewicz/java8/J04_FunctionalInterfacesTest.java
@@ -27,7 +27,7 @@ public class J04_FunctionalInterfacesTest {
 	public void testActionListenerLambda() {
 		//given
 		final Date dateMock = mock(Date.class);
-		final ActionListener listener = null;
+		final ActionListener listener = x -> dateMock.setTime(1000);
 
 		//when
 		listener.actionPerformed(null);
@@ -40,7 +40,7 @@ public class J04_FunctionalInterfacesTest {
 	public void testRunnableLambda() {
 		//given
 		final Date dateMock = mock(Date.class);
-		Runnable block = null;
+		Runnable block = () -> dateMock.setTime(1000);
 
 		//when
 		block.run();
@@ -51,7 +51,7 @@ public class J04_FunctionalInterfacesTest {
 
 	@Test
 	public void testComparatorLambda() {
-		final Comparator<String> strLenComparator = null;
+		final Comparator<String> strLenComparator = Comparator.comparingInt(String::length);
 
 		assertThat(strLenComparator.compare("abc", "def")).isZero();
 		assertThat(strLenComparator.compare("abc", "defg")).isLessThan(0);
@@ -60,9 +60,9 @@ public class J04_FunctionalInterfacesTest {
 
 	@Test
 	public void testCustomFunctionalInterface() {
-		final RandomSource source = null;
+		final RandomSource source = () -> random.nextInt(2) * 2 - 1;
 
-		Supplier<Integer> sourceSupplier = null;
+		Supplier<Integer> sourceSupplier = source::oneOrMinusOne;
 
 		assertThat(source.oneOrMinusOne()).isIn(-1, 1);
 		assertThat(sourceSupplier.get()).isIn(-1, 1);
